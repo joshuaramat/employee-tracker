7178518402840341a4e6303a00323ba0b9c3787d
joshuaramat <87837577+joshuaramat@users.noreply.github.com>
2021-10-18 12:10:50 -0700

    added seed.sql

--- schema.sql
+++ schema.sql
@@ -3,22 +3,28 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employee(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role INTEGER FOREIGN KEY NOT NULL,
-    manager_id INTEGER FOREIGN KEY NOT NULL
+CREATE TABLE department(
+    id INTEGER AUTO_INCREMENT,
+    name VARCHAR(70) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2) INTEGER FOREIGN KEY NOT NULL,
-    department INTEGER FOREIGN KEY NOT NULL
+    id INTEGER AUTO_INCREMENT,
+    title VARCHAR(70) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER FOREIGN KEY NOT NULL
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE department(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(30) NOT NULL
+CREATE TABLE employee(
+    id INTEGER AUTO_INCREMENT,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
